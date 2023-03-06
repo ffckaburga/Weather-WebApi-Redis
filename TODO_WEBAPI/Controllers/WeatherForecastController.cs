1604fdc6515cec8e5169142fa1ae67bd210e2ca5
fuatck <ffkaburga@gmail.com>
2023-03-06 15:40:09 +0300

    the version that redis-cache was not implemented

---
+++ TODO_WEBAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODO_WEBAPI.Models;
+
+namespace TODO_WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // tek şehir
+        [HttpGet]
+        public HavaDurumu Get(string city, string country)
+        {
+            using (var client = new System.Net.WebClient())
+            {
+                client.Encoding = System.Text.Encoding.UTF8;
+                var responseString = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=metric&appid=1ef396acb73a49b95b2a829b1b085e31");
+                var hava = JsonConvert.DeserializeObject<HavaDurumu>(responseString);
+                hava.weather[0].icon = @"http://openweathermap.org/img/w/" + hava.weather[0].icon + ".png";
+                return hava;
+            }
+        }
+
+        // birden fazla şehir
+        [HttpPost]
+        public List<HavaDurumu> GetWeather([FromBody] List<Location> loc)
+        {
+            List<HavaDurumu> havaDurumuList = new List<HavaDurumu>();
+            foreach (var item in loc)
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    client.Encoding = System.Text.Encoding.UTF8;
+
+                    var responseString = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + item.city + "," + item.country + "&units=metric&appid=1ef396acb73a49b95b2a829b1b085e31");
+
+                    if (responseString != null)
+                    {
+                        var hava = JsonConvert.DeserializeObject<HavaDurumu>(responseString);
+                        hava.weather[0].icon = @"http://openweathermap.org/img/w/" + hava.weather[0].icon + ".png";
+                        havaDurumuList.Add(hava);
+                    }
+
+                }
+
+            }
+            return havaDurumuList;
+        }
+
+
+
+    }
+}
---
+++ TODO_WEBAPI/Models/HavaDurumu.cs
@@ -0,0 +1,39 @@
+﻿namespace TODO_WEBAPI.Models
+{
+    public class HavaDurumu
+    {
+        public Weather[] weather { get; set; }
+        public Main main { get; set; }
+        public Sys sys { get; set; }
+        public string name { get; set; }
+        public string timezone { get; set; }
+    }
+
+    public class Weather
+    {
+        public string description { get; set; }
+        public string icon { get; set; }
+
+    }
+    public class Main
+    {
+        public string humidity { get; set; }
+        public string temp { get; set; }
+        public string temp_min { get; set; }
+        public string temp_max { get; set; }
+
+    }
+
+
+    public class Location
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+    }
+    
+
+    public class Sys
+    {
+        public string country { get; set; }
+    }
+}
